b06133e23bdbe29acedc99406b3a7294331d4bca
Christina Hong <hongchristinaaa@gmail.com>
2022-09-24 20:41:50 -0700

    Contact form has to be fully completed before submitting

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -16,14 +16,18 @@ export default function Contact() {
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    if(name && message && email) {
+      e.preventDefault();
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", name, email, message }),
+      })
+        .then(() => alert("Message sent!"))
+        .catch((error) => alert(error));
+    } else {
+      alert('Please fill out all information before submitting')
+    }
   }
   
   return (
